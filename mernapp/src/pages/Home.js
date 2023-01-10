07ac82bacea79d82e1029011d5b4f57a58b53b22
Mohammad Shafi <shafi.bahrami.2015@gmail.com>
2023-01-10 23:09:13 +0100

    Adding cards

--- mernapp/src/pages/Home.js
+++ mernapp/src/pages/Home.js
@@ -1,14 +1,38 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
-const Home=()=>{
+const Home = () => {
    return (
       <div>
          <div>
-            <Navbar/>
+            <Navbar />
          </div>
-         Body
-         <Footer/>
+         <div>
+            <div class="card mt-3" style={{"width": "18rem","maxHeight":"360px"}}>
+               <img class="card-img-top" src="..." alt="Card image cap"/>
+                  <div class="card-body">
+                     <h5 class="card-title">Card title</h5>
+                     <p class="card-text">Text</p>
+                     <div className="container w-100">
+                        <select className="m-2 h-100 w-100 bg-success">
+                           {Array.from(Array(6),(e,i)=>{
+                              return (
+                                 <option
+                                    key={i+1}
+                                    value={i+1}
+                                 >
+                                    {i+1}
+                                 </option>
+                              )
+                           })}
+                        </select>
+                     </div>
+                  </div>
+            </div>
+         </div>
+         <div>
+            <Footer />
+         </div>
       </div>
    )
 }
