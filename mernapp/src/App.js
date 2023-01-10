7f76bb4dad30fe83d456e881d1ce7ec4edf0aaec
Mohammad Shafi <shafi.bahrami.2015@gmail.com>
2023-01-10 22:53:59 +0100

    Adding static pages

--- mernapp/src/App.js
+++ mernapp/src/App.js
@@ -1,8 +1,19 @@
+import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+
 function App() {
   return (
-     <div>
-      App
-     </div>
+     <Routes>
+       <Route
+          exact path="/"
+          element={<Home/>}
+       />
+       <Route
+          path="/login"
+          element={<Login/>}
+       />
+     </Routes>
   );
 }
 
---
+++ mernapp/src/pages/Home.js
@@ -0,0 +1,16 @@
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+const Home=()=>{
+   return (
+      <div>
+         <div>
+            <Navbar/>
+         </div>
+         Body
+         <Footer/>
+      </div>
+   )
+}
+
+export default Home;
