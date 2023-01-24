f53a9f193312486d247253d74f23d136f4bb20fe
Mohammad Shafi <shafi.bahrami.2015@gmail.com>
2023-01-24 22:04:25 +0100

    Adding dishes + updating restaurant

--- mernapp/backend/controllers/foodItems.js
+++ mernapp/backend/controllers/foodItems.js
@@ -1,10 +1,18 @@
 const foodItems=require('../models/Food_items');
 const createError=require('../middlewares/createError');
+const Restaurant=require('../models/RestaurantModel');
 
 const createItems=async(req,res,next)=>{
+   const {restaurantId:id}=req.params;
    try{
       const item=new foodItems({...req.body});
       await item.save();
+      try{
+         await Restaurant.findByIdAndUpdate(id,{$push:{dishes:item._id}},{new:true});
+      }
+      catch(err){
+         return next(createError(400,'Both email and password are required'));
+      }
       res.status(200).json({success:true,item});
    }
    catch(err){
--- mernapp/backend/routes/Food_items.js
+++ mernapp/backend/routes/Food_items.js
@@ -3,6 +3,7 @@ const router=express.Router();
 const {createItems,getItems}=require('../controllers/foodItems');
 const {verifyToken}=require('../middlewares/verifyToken');
 
-router.route('/').post(createItems).get(getItems);
+router.route('/').get(getItems);
+router.route('/:restaurantId').post(createItems);
 
 module.exports=router;
